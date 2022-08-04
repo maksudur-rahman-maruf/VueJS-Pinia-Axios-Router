cbc735c3665e05005af7e27ffa29bcc26a96ea43
maksudur-rahman-maruf <maruf.bsnm@gmail.com>
2022-08-04 23:46:10 +0600

    Health Check URL TESTED

--- src/stores/post.js
+++ src/stores/post.js
@@ -38,7 +38,10 @@ export const usePostStore = defineStore({
       this.loading = true
       try {
         let response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        this.post = response.data
+        this.post = response.data;
+        let response2 = await axios.get(`http://YOUR_APP_BASE_URL:3002/api/v1/healthcheck`);
+        console.log(response2.data);
+
         // this.post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         // .then((response) => response.json())
       } catch (error) {
